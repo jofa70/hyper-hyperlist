58dbc78113287ea1f19997a5bf1f90f4ffc902f4
jofa70 <jofa@outlook.com>
2018-06-20 17:46:56 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { app } from 'hyperapp'
+
+import state from './js/state.js'
+import actions from './js/actions.js'
+import view from './js/view.js'
+
+const main = app(state, actions, view, document.body);
